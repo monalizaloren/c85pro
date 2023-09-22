5cff3852ab1d638c80e9231e6dae6d3922beb81e
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-09-22 20:18:38 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBffclVqQe9XrrJ0kFLBU4mN8yDClpiMWc",
+  authDomain: "historias-9891d.firebaseapp.com",
+  projectId: "historias-9891d",
+  storageBucket: "historias-9891d.appspot.com",
+  messagingSenderId: "201609685013",
+  appId: "1:201609685013:web:0f4854af5335d986ebc61c",
+  measurementId: "G-1HVF8M63C9"
+};
